54e144668ca410208057f143b929219dd99b2b1a
Franco Camusso <116894094+FrancoCamusso97@users.noreply.github.com>
2023-06-28 10:15:36 -0300

    apibase  environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
   production: true
+   apiBaseUrl: 'https://backend-rrgx.onrender.com',
+  firebaseConfig: {
+    apiKey: "AIzaSyBhqna2qjGJbQnHjqiRsGOacxIfJ9xof-k",
+    authDomain: "login-6ae3e.firebaseapp.com",
+    projectId: "login-6ae3e",
+    storageBucket: "login-6ae3e.appspot.com",
+    messagingSenderId: "502768075530",
+    appId: "1:502768075530:web:6b26083187f25ff8a730f3"
+  }
 };
